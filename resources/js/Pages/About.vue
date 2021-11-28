b3b672b7d3bf25d5917c6ffe885f67dcf3c5346f
Rechie Yee <yeeopaw@gmail.com>
2021-11-28 23:57:17 +0800

    ui about us page

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,34 +1,104 @@
 <template>
     <app-header></app-header>
 
-    <div  class="flex flex-col mx-auto h-screen lg:px-36 px-2" >
-        <div  class="flex flex-col h-full">
-            <div class="h-5/6 bg-white">
-                <div class="flex lg:flex-row flex-col h-full">
-                    <div class="lg:w-1/2 w-full relative " >
+    <div  class=" mx-auto h-auto lg:px-36 px-2 mt-1"  >
+               <div class="h-auto bg-white z-0">
+                <div class="flex lg:flex-row flex-col ">
+                    <div class="lg:w-1/2 w-full relative h-auto" >
                         <div class="absolute py-16 pl-16 pr-20 ml-16 h-3/4 -right-16 top-20 bottom-20 bg-white-o-70">     
-                        
-                            <div class="mb-8 border-l-4 border-gray-500">
+                            <div class="h-full">
+                                    <div class="mb-8 border-l-4 border-gray-500 relative">
                                 <h2 class="ml-5 brand-text-header text-3xl font-semibold">MEET THE CEO OF</h2>
                                 <h2 class="ml-5 brand-logo-bold text-3xl font-semibold">GLI <span class="brand-logo-light font-thin">GROUP INC.</span></h2>    
                             </div>               
-                                <p class="text-left text-xl brand-text-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p>
+                                <p class="text-left text-xl brand-text-content relative">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p>
+                            </div>
+                            
                         </div>               
                     </div>
-                    <div class="lg:w-1/2 w-full bg-local" style="background-image: url('/images/pages/ceo.jpeg')"></div>
+                    <div class="lg:w-1/2 w-full h-3/4">
+                    <img src="/images/pages/ceo.jpg" class="object-scale-down " alt="">
+                    </div>
+                </div>
+        </div>
+    </div>
+    <div class="h-auto w-full bg-transparent" >
+            <div class="flex lg:flex-row flex-col  bg-contain bg-center bg-local " style="background-image: url('/images/pages/house.jpg')" >
+                <div class="w-full relative py-10 h-auto bg-green-o-70" >
+                    <div class="py-4 h-full text-center mt-auto w-full flex flex-col justify-center text-white bg-transparent">
+                        <h2 class="ml-5 brand-text-content text-3xl font-light">We Build as One here in</h2>
+                        <h2 class="ml-5 brand-logo-bold text-4xl font-bold">GLI <span class="brand-logo-light font-thin">GROUP INC.</span></h2>    
+                    </div> 
                 </div>
             </div>
-
-            <div class="h-1/3 bg-cover bg-bottom bg-local" style="background-image: url('/images/pages/house.jpeg')" >
-                <div class="py-4 h-full text-center mt-auto w-full flex flex-col justify-center text-white bg-green-o-70">
-                    <h2 class="ml-5 brand-text-content text-3xl font-light">We Build as One here in</h2>
-                    <h2 class="ml-5 brand-logo-bold text-4xl font-bold">GLI <span class="brand-logo-light font-thin">GROUP INC.</span></h2>    
-                </div> 
-            
-            </div>
-
-        </div>
+    </div>
         
+        <div  class=" mx-auto h-auto lg:px-36 px-2"  >
+               <div class="h-auto bg-white z-0">
+                <div class="flex lg:flex-row flex-col ">
+                    <div class="lg:w-1/2 w-full h-3/4 py-16">
+                         <img src="/images/pages/mission.jpg" class="object-scale-down " alt="">
+                    </div>
+                    <div class="lg:w-1/2 w-full  h-auto" >
+                        <div class=" py-16 pl-16 pr-20 ml-16 h-3/4 -right-16 top-20 bottom-20 bg-white-o-70">     
+                            <div class="h-full">
+                                    <div class="mb-8 border-l-4 border-gray-500 relative">
+                                <h2 class="ml-5 brand-text-header text-3xl font-semibold">OUR COMPANY'S</h2>
+                                <h2 class="ml-5 brand-logo-bold text-3xl font-semibold">MISSION <span class="brand-logo-light font-thin"></span></h2>    
+                            </div>               
+                                <p class="text-left text-xl brand-text-content relative">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p>
+                            </div>
+                            
+                        </div>               
+                    </div>
+                    
+                </div>
+        </div>
+    </div>
+            <div  class=" mx-auto h-auto lg:px-36 px-2"  >
+               <div class="h-auto bg-white z-0">
+                <div class="flex lg:flex-row flex-col ">
+                    
+                    <div class="lg:w-1/2 w-full  h-auto" >
+                        <div class=" py-16 pl-16 pr-20 ml-16 h-3/4 -right-16 top-20 bottom-20 bg-white-o-70">     
+                            <div class="h-full">
+                                    <div class="mb-8 border-l-4 border-gray-500 relative">
+                                <h2 class="ml-5 brand-text-header text-3xl font-semibold">OUR COMPANY'S</h2>
+                                <h2 class="ml-5 brand-logo-bold text-3xl font-semibold">VISION <span class="brand-logo-light font-thin"></span></h2>    
+                            </div>               
+                                <p class="text-left text-xl brand-text-content relative">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p>
+                            </div>
+                            
+                        </div>               
+                    </div>
+                    <div class="lg:w-1/2 w-full h-3/4 py-16">
+                         <img src="/images/pages/vision.jpg" class="object-scale-down " alt="">
+                    </div>
+                    
+                </div>
+        </div>
+    </div>
+     <div  class=" mx-auto h-auto lg:px-36 px-2"  >
+               <div class="h-auto bg-white z-0">
+                <div class="flex lg:flex-row flex-col ">
+                    <div class="lg:w-1/2 w-full h-3/4 py-16">
+                         <img src="/images/pages/value.jpg" class="object-scale-down " alt="">
+                    </div>
+                    <div class="lg:w-1/2 w-full  h-auto" >
+                        <div class=" py-16 pl-16 pr-20 ml-16 h-3/4 -right-16 top-20 bottom-20 bg-white-o-70">     
+                            <div class="h-full">
+                                    <div class="mb-8 border-l-4 border-gray-500 relative">
+                                <h2 class="ml-5 brand-text-header text-3xl font-semibold">OUR COMPANY'S</h2>
+                                <h2 class="ml-5 brand-logo-bold text-3xl font-semibold">VALUE <span class="brand-logo-light font-thin"></span></h2>    
+                            </div>               
+                                <p class="text-left text-xl brand-text-content relative">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p>
+                            </div>
+                            
+                        </div>               
+                    </div>
+                    
+                </div>
+        </div>
     </div>
     <app-footer></app-footer>
 </template>
