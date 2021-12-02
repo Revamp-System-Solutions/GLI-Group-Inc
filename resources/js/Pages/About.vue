f93929360399d8ba47d33275a277d5f6af1a0721
Alamein Tabunda <alameinptabunda@su.edu.ph>
2021-12-03 03:34:58 +0800

    color switch bg contact

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -2,19 +2,19 @@
     <app-header></app-header>
 
     <div  class="h-auto 2xl:px-80 xl:px-56 lg:px-28 "  >
-        <div class="flex lg:flex-row  flex-col-reverse h-auto z-0 relative">
-            <div class="w-full  h-3/4 lg:py-36 lg:pl-0 p-6  static" >
+        <div class="flex lg:flex-row flex-col-reverse h-auto z-0 relative">
+            <div class="w-full h-3/4 lg:py-36 lg:pl-0 p-6 static" >
                 <div class=" lg:py-36 lg:my-12 static">
                     <div class="bg-white bg-opacity-70 lg:absolute lg:top-0 xl:pr-32 lg:pr-28 lg:py-16 lg:my-20  lg:w-7/12  w-full ">  
                         <div class="mb-8 border-l-4 border-gray-500 ">
                             <h2 class="ml-5 brand-text-header lg:text-3xl text-xl font-semibold">MEET THE CEO OF</h2>
                             <h2 class="ml-5 brand-logo-bold text-3xl font-semibold">GLI <span class="brand-logo-light font-thin text-green-600">GROUP INC.</span></h2>  
                         </div>               
-                        <p class="text-justify 2xl:text-lg  text-sm brand-text-content lg:pr-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
+                        <p class="text-justify 2xl:text-lg text-sm brand-text-content lg:pr-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
                     </div>            
                 </div>               
             </div>
-            <div class="w-full lg:h-auto h-96 static bg-cover bg-top-right bg-local " style="background-image: url('/images/pages/ceo.jpg')" >
+            <div class="w-full lg:h-auto h-96 static bg-cover bg-top-right bg-local" style="background-image: url('/images/pages/ceo.jpg')" >
                         &nbsp;
             </div>
         </div>
@@ -32,36 +32,36 @@
         
 
     <div  class="h-auto 2xl:px-80 xl:px-56 lg:px-28 "  >
-        <div class="flex lg:flex-row-reverse  flex-col-reverse h-auto">
-            <div class="w-full  h-2/3 lg:py-20 lg:pl-10 lg:pr-0 p-6" >            
+        <div class="flex lg:flex-row-reverse flex-col-reverse h-auto">
+            <div class="w-full h-2/3 lg:py-20 lg:pl-10 lg:pr-0 p-6" >            
                     <div class="w-full ">  
                         <div class="mb-8 font-bold uppercase">
                             <h2 class="brand-text-header text-3xl">OUR COMPANY'S</h2>
                             <h2 class="brand-text-bold text-3xl text-green-600">MISSION</h2>  
                         </div>               
-                        <p class="text-justify 2xl:text-lg  text-sm brand-text-content ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
+                        <p class="text-justify 2xl:text-lg text-sm brand-text-content ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
                     </div>                       
             </div>
             <div class="w-full lg:h-auto lg:py-10 py-5 lg:pr-10" >
-                <div class="w-full lg:h-full h-96 bg-cover bg-top bg-local " style="background-image: url('/images/pages/mission.jpg')">
+                <div class="w-full lg:h-full h-96 bg-cover bg-top bg-local" style="background-image: url('/images/pages/mission.jpg')">
                         &nbsp;
                 </div>    
             </div>
         </div>
     </div>
     <div  class="h-auto 2xl:px-80 xl:px-56 lg:px-28 "  >
-        <div class="flex lg:flex-row  flex-col-reverse h-auto">
-            <div class="w-full  h-2/3 lg:py-20 lg:pr-10 lg:pl-0 p-6" >            
+        <div class="flex lg:flex-row flex-col-reverse h-auto">
+            <div class="w-full h-2/3 lg:py-20 lg:pr-10 lg:pl-0 p-6" >            
                     <div class="w-full ">  
                         <div class="mb-8 font-bold uppercase">
                             <h2 class="brand-text-header text-3xl">OUR COMPANY'S</h2>
                             <h2 class="brand-text-bold text-3xl text-green-600">Vision</h2>  
                         </div>               
-                        <p class="text-justify 2xl:text-lg  text-sm brand-text-content ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
+                        <p class="text-justify 2xl:text-lg text-sm brand-text-content ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
                     </div>                       
             </div>
             <div class="w-full lg:h-auto lg:py-10 py-5 lg:pl-10" >
-                <div class="w-full lg:h-full h-96 bg-cover bg-top bg-local " style="background-image: url('/images/pages/vision.jpg')">
+                <div class="w-full lg:h-full h-96 bg-cover bg-top bg-local" style="background-image: url('/images/pages/vision.jpg')">
                         &nbsp;
                 </div>    
             </div>
@@ -69,8 +69,8 @@
     </div>
 
    <div  class="h-auto 2xl:px-80 xl:px-56 lg:px-28" >
-        <div class="flex lg:flex-row-reverse  flex-col-reverse h-auto">
-            <div class="w-full  h-2/3 lg:py-20 lg:pl-10 lg:pr-0 p-6" >            
+        <div class="flex lg:flex-row-reverse flex-col-reverse h-auto">
+            <div class="w-full h-2/3 lg:py-20 lg:pl-10 lg:pr-0 p-6" >            
                     <div class="w-full ">  
                         <div class="mb-8 font-bold uppercase">
                             <h2 class="brand-text-header text-3xl">OUR COMPANY'S</h2>
@@ -79,8 +79,8 @@
                         <p class="text-justify 2xl:text-lg  text-sm brand-text-content ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque egestas nec ipsum ac mollis. Duis at lorem sem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur felis lacus, venenatis non maximus vitae, laoreet sit amet risus. Vivamus id ex magna. Vestibulum nibh elit, vestibulum ac mauris a, ornare sodales lorem. Aenean turpis velit, hendrerit eget dui sed, accumsan molestie libero. Donec sapien magna, egestas vel congue vel, accumsan et erat. Etiam nisi odio, pretium sit amet nunc at, dapibus vehicula augue.</p> 
                     </div>                       
             </div>
-            <div class="w-full lg:h-auto lg:py-10 py-5  lg:pr-10" >
-                <div class="w-full lg:h-full h-96 bg-cover bg-top bg-local " style="background-image: url('/images/pages/value.jpg')">
+            <div class="w-full lg:h-auto lg:py-10 py-5 lg:pr-10" >
+                <div class="w-full lg:h-full h-96 bg-cover bg-top bg-local" style="background-image: url('/images/pages/value.jpg')">
                         &nbsp;
                 </div>    
             </div>
@@ -89,7 +89,7 @@
 
 
     <div class="h-auto w-full bg-transparent" >
-            <div class="flex lg:flex-row flex-col  bg-cover bg-center bg-local " style="background-image: url('/images/pages/groups.jpeg')" >
+            <div class="flex lg:flex-row flex-col bg-cover bg-center bg-local" style="background-image: url('/images/pages/groups.jpeg')" >
                 <div class="w-full relative py-10 h-auto " >
                     <div class="py-4 h-56 text-center mt-auto w-full flex flex-col justify-center text-white bg-transparent">
                     </div> 
