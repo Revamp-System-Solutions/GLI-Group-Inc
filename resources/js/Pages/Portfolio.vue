1d3b763e29b77f808dbc26be566e6db357b06ec6
Alamein Tabunda <alameinptabunda@su.edu.ph>
2021-12-02 20:34:51 +0800

    portfolio hover full

--- resources/js/Pages/Portfolio.vue
+++ resources/js/Pages/Portfolio.vue
@@ -1,32 +1,25 @@
 <template>
     <app-header></app-header>
-    
-    <div  class=" mx-auto h-auto "  >
-        <div class="flex lg:flex-row-reverse  flex-col-reverse h-auto">
-            <div class="w-full  h-auto flex" >            
-                    <div class="transition duration-500 ease-in-out bg-blue-600 flex-grow text-center h-auto bg-local bg-cover bg-center" style="background-image: url('/images/pages/design.jpg')" id="designstudio" :class="portfolioCats.designstudio ? 'w-10/12 ':'w-1/3'" @mouseover="expandHovered('designstudio')" @mouseleave="shrinkHovered('designstudio')">  
-                        <div class="font-bold uppercase h-full bg-green-600 bg-opacity-70 p-36" id="pcChild-1">
-                            <h2 class="brand-logo-bold text-xl font-semibold text-black">GLI</h2>
-                            <h2 class="brand-logo-light text-3xl text-white font-light">Design Studio</h2>  
-                        </div>               
-                    </div>
-                    <div class="transition duration-500 ease-in-out bg-blue-600 flex-grow text-center h-auto bg-local bg-cover bg-center" style="background-image: url('/images/pages/builders.jpg')" id="builders" :class="portfolioCats.builders ? 'w-10/12 ':'w-1/3'" @mouseover="expandHovered('builders')" @mouseleave="shrinkHovered('builders')">  
-                        <div class="font-bold uppercase h-full bg-green-600 bg-opacity-70 p-36" id="pcChild-2">
-                            <h2 class="brand-logo-bold text-xl font-semibold text-black">GLI</h2>
-                            <h2 class="brand-logo-light text-3xl text-white font-light">Builders</h2>  
-                        </div>               
-                    </div> 
-                    <div class="transition duration-500 ease-in-out bg-blue-600 flex-grow text-center h-auto bg-local bg-cover bg-center" style="background-image: url('/images/pages/modularcabs.jpg')" id="modularcabinets" :class="portfolioCats.modularcabinets ? 'w-10/12 ':'w-1/3'"  @mouseover="expandHovered('modularcabinets')" @mouseleave="shrinkHovered('modularcabinets')">  
-                        <div class="font-bold uppercase h-full bg-green-600 bg-opacity-70 p-36" id="pcChild-3">
-                            <h2 class="brand-logo-bold text-xl font-semibold text-black">GLI</h2>
-                            <h2 class="brand-logo-light text-3xl text-white font-light">Modular Cabinets</h2>  
-                        </div>               
-                    </div>                        
-            </div>
-
+        <div class="transition duration-500 ease-in-out w-full h-auto flex overflow-hidden relative" >            
+                <div class="transition-width transition-slowest ease cursor-pointer h-auto bg-local bg-cover bg-center static w-1/3" style="background-image: url('/images/pages/design.jpg')" id="designstudio" @mouseover="expandHovered('designstudio')">  
+                    <div class="static transition duration-500 ease-in-out uppercase text-center lg:h-48 h-96 bg-green-600 bg-opacity-70 " id="pcChild-1"> 
+                        <h2 class="transition duration-500 ease-in-out brand-logo-light lg:text-3xl text-base text-white font-light  transform origin-center lg:-translate-x-0 lg:translate-y-20 translate-y-48">Design Studio</h2>      
+                    </div>               
+                </div>
+                <div class="transition-width transition-slowest ease cursor-pointer h-auto bg-local bg-cover bg-center static w-1/3" style="background-image: url('/images/pages/builders.jpg')" id="builders" @mouseover="expandHovered('builders')" >  
+                    <div class="static transition duration-500 ease-in-out uppercase text-center lg:h-48 h-96 bg-green-600 bg-opacity-70 " id="pcChild-2">
+                        <h2 class="transition duration-500 ease-in-out brand-logo-light lg:text-3xl text-base text-white font-light  transform origin-center lg:-translate-x-0 lg:translate-y-20 translate-y-48">Builders</h2>  
+                    </div>               
+                </div> 
+                <div class="transition-width transition-slowest ease cursor-pointer h-auto bg-local bg-cover bg-center static w-1/3" style="background-image: url('/images/pages/modularcabs.jpg')" id="modularcabinets" @mouseover="expandHovered('modularcabinets')">  
+                    <div class="static transition duration-500 ease-in-out uppercase text-center lg:h-48 h-96 bg-green-600 bg-opacity-70 " id="pcChild-3">
+                        <h2 class="transition duration-500 ease-in-out brand-logo-light lg:text-3xl text-base text-white font-light  transform origin-center lg:-translate-x-0 lg:translate-y-20 translate-y-48">Modular Cabinets</h2>  
+                    </div>               
+                </div>                        
         </div>
-    </div>
-
+        <div v-if="hovered != null">
+                {{hovered}}            
+        </div>
     <app-footer></app-footer>
 </template>
 
@@ -44,34 +37,35 @@ export default {
         AppHeader,
         AppFooter
     },
-    data: () =>({
-        portfolioCats: {'designstudio':false,'builders':false,'modularcabinets':false}
-    }),
     props: {
         errors: Object
     },
+    data: () =>({
+        portfolioCats: {'designstudio':false,'builders':false,'modularcabinets':false},
+        hovered: null
+
+    }),
+    
     methods:{
         expandHovered: function(id){
-            this.portfolioCats[id]=true
+
             for(var pc in this.portfolioCats){
+                this.portfolioCats[pc]=false
                 if(id!==pc){
-                $("#"+pc).addClass("w-1/12");
-                $("#"+$("#"+pc).children('div').attr('id')).removeClass('bg-green-600 p-36').addClass('bg-green-900')
-                 $("#"+$("#"+pc).children('div').attr('id')).children('h2').hide()
+                    $("#"+pc).removeClass('w-10/12')
+                    $("#"+pc).removeClass('w-1/3').addClass("w-1/12 ");          
+                    $("#"+$("#"+pc).children('div').attr('id')).removeClass('bg-green-600 ').addClass('bg-green-900')
+                    $("#"+$("#"+pc).children('div').attr('id')).children('h2').removeClass('rotate-0 translate-y-48 lg:text-3xl').addClass('-rotate-90 translate-y-60 lg:text-xs whitespace-nowrap')
+                }else{
+                    this.hovered = pc;     
+                    this.portfolioCats[pc]=true
+                    $("#"+pc).removeClass('w-1/3').addClass('w-10/12');
+                    $("#"+$("#"+pc).children('div').attr('id')).removeClass('bg-green-900').addClass('bg-green-600 ')
+                    $("#"+$("#"+pc).children('div').attr('id')).children('h2').removeClass('-rotate-90 translate-y-60 lg:text-xs whitespace-nowrap').addClass('rotate-0 translate-y-48 lg:text-3xl')
                 }
             }
         },
-         shrinkHovered: function(id){
-           
-            for(var pc in this.portfolioCats){
-                $("#"+pc).removeClass("w-1/12");
-                 this.portfolioCats[pc]=false
-                
-                $("#"+$("#"+pc).children('div').attr('id')).removeClass('bg-green-900').addClass('bg-green-600 p-36')
-                 $("#"+$("#"+pc).children('div').attr('id')).children('h2').show()
-               
-            }
-        }
+
     }
    
 }
