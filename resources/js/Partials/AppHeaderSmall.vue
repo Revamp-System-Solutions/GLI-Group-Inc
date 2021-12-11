7b633ed7767a6c97fb3b1792621f496a8bb1da3e
Alamein Tabunda <alameinptabunda@su.edu.ph>
2021-12-11 11:01:26 +0800

    remove header link

--- resources/js/Partials/AppHeaderSmall.vue
+++ resources/js/Partials/AppHeaderSmall.vue
@@ -221,7 +221,7 @@ export default {
 			},
 			{
 				name: 'Users',
-				href: 'media.create',
+				href: 'admin.settings',
 				icon: 'fas fa-users',
 				order: 'order-2',
 				parentLink: 'Site'
