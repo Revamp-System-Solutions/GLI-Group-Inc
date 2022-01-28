a812d5893db66f1992a4dfeb22369a968dcb4b05
Alamein Tabunda <alameinptabunda@su.edu.ph>
2022-01-28 22:25:48 +0800

    Sol 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Events\MigrationsStarted;
+use Illuminate\Database\Events\MigrationsEnded;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,10 +17,21 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        //
-    }
+     public function register() {
+    
+            // check this one here https://github.com/laravel/framework/issues/33238#issuecomment-897063577
+            Event::listen(MigrationsStarted::class, function (){
+                if (config('databases.allow_disabled_pk')) {
+                    DB::statement('SET SESSION sql_require_primary_key=0');
+                }
+            });
+    
+            Event::listen(MigrationsEnded::class, function (){
+                if (config('databases.allow_disabled_pk')) {
+                    DB::statement('SET SESSION sql_require_primary_key=1');
+                }
+            });
+        } 
 
     /**
      * Bootstrap any application services.
