3436fa6c018316961b2c6f125d874f028509e11d
Alamein <admin@gligroupinc.com>
2022-07-01 23:34:24 +0800

    Success Sendmail

--- app/Mail/NotifyMail.php
+++ app/Mail/NotifyMail.php
@@ -28,6 +28,6 @@ class NotifyMail extends Mailable
      */
     public function build()
     {
-        return $this->view('email.responseMail');
+        return $this->view('emails.responseMail');
     }
 }
