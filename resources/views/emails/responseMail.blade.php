bf0dd592b42ee0a1a0612495e9b96a7ab1571add
alokeith <keithkeithkeith2000@gmail.com>
2022-07-25 01:32:09 +0800

    responseMail edit

--- resources/views/emails/responseMail.blade.php
+++ resources/views/emails/responseMail.blade.php
@@ -1,25 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
 
 <head>
-    <title>GLI GROUP INC.</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="x-apple-disable-message-reformatting">
+    <title></title>
+    <!--[if mso]>
+  <noscript>
+    <xml>
+      <o:OfficeDocumentSettings>
+        <o:PixelsPerInch>96</o:PixelsPerInch>
+      </o:OfficeDocumentSettings>
+    </xml>
+  </noscript>
+  <![endif]-->
+    <style>
+    h1,
+    p {
+        font-family: Arial, sans-serif;
+    }
+    </style>
 </head>
 
 <body>
+    <div class="flex flex-col items-center justify-center">
+        <div class="w-2/5 border-2">
+            <div class="bg-white shadow-2xl flex items-center justify-center" style="margin-bottom:50px;">
+                <img src="https://1000logos.net/wp-content/uploads/2021/05/Jollibee-logo.png" class="w-96 m-1"></img>
+            </div>
+            <div class="bg-gray-100">
+                <h1 class="text-black m-6 text-2xl font-bold antialiased ">Thank you for getting in touch with us!</h1>
+                <p class="m-6">We appreciate you contacting GLI Group Inc. One of our Professionals will get back in
+                    touch with you soon! Have a great day!</p>
+                <a href="" class="m-6 text-green-800 font-semibold underline">Click here to Subscribe</a>
+                <br><br><br><br><br>
+            </div>
+            <div class="bg-green-600 flex ">
+                <div class="m-6 flex flex-col">
+                    <p class="text-white m-0">® Someone, Somewhere 2021</p>
+                    <a href="" class="text-white m-0 underline">Unsubscribe</a>
+                </div>
 
-    <h1 class="text-3xl font-bold underline"> Thank you for getting in touch!</h1>
-    <p>
-        We appreciate you contacting GLI Group Inc.. One of our Professionals will get back in touch with you soon! Have a great day!
-    </p>
-    <img
-          class="max-w-xs w-28"
-          src="/images/brand-logo-small.png"
-          alt="Revamp Logo"
-          style=""
-        />
+                <div class="m-auto flex" style="padding-left: 190px;">
+                    <a href="http://www.twitter.com/" class="block h-auto w-10 m-1"><img
+                            src="https://assets.codepen.io/210284/tw_1.png" />
+                        <a href="http://www.facebook.com/" class="block h-auto w-10 m-1"><img
+                                src="https://assets.codepen.io/210284/fb_1.png" />
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
+
 
 </html>
